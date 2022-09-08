d84a6e3dcd6949286b19a3ffab3ab281c11a6c7c
Gio <69811809+aiellog2@users.noreply.github.com>
2022-09-08 19:36:43 -0400

    Alpha0.1

---
+++ HorizonAlpha1/Assets/Scripts/SceneChangeto3.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeto3 : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
