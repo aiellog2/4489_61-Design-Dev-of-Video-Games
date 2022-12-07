ca77b54ff9dedd79548322017852e71189f1056b
Gio <69811809+aiellog2@users.noreply.github.com>
2022-12-07 14:15:31 -0500

    re_main

---
+++ HorizonAlpha1/Assets/Scripts/NPC.cs
@@ -0,0 +1,96 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEditor.Timeline.Actions;
+
+
+public class NPC : MonoBehaviour
+{
+    public InputReader inputreader;
+
+    public GameObject dialoguePanel;
+    public GameObject contButton;
+    public GameObject sprite;
+    public Text dialogueText;
+    public string[] dialogue;
+
+    private int index;
+
+    public float wordSpeed;
+    public bool playerCollided;
+
+
+    void Update()
+    {
+        if (playerCollided)
+        {
+            sprite.SetActive(true);
+        }
+        else
+        {
+            sprite.SetActive(false);
+        }
+        if(playerCollided && Input.GetKeyDown(KeyCode.E))
+        {
+
+            Cursor.visible = true;
+
+            if (dialoguePanel.activeInHierarchy)
+            {
+                ZeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        if(dialogueText.text == dialogue[index])
+        {
+            contButton.SetActive(true);
+        }
+    }
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text+=letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+    public void NextLine()
+    {
+        contButton.SetActive(false);
+
+        if(index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text="";
+            StartCoroutine(Typing());
+        }
+    }
+    public void ZeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerCollided = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCollided = false;
+            ZeroText();
+        }
+    }
+}
